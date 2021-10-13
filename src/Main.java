e4cd7984286126027053e8659debfee70a9109ad
Roman Medvedev <romankazan88@gmail.com>
2021-10-13 18:48:14 +0300

    balance

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 200;
+        int amount = 1500;
+        int bonus = 1 * amount / 100 + (amount + balance);
+
+
+
+            if (amount >= 1000) {
+                bonus = 1 * amount / 100 +(amount+balance);
+            }
+            if (amount < 1000) {
+                bonus = amount+balance;
+            }
+
+            System.out.println(bonus);
+
+
+        }
+}
